91ccb0d5ad04232fcd9845c5b75d40bc84acf734
Takahiko Inayama <sagittariusm25@gmail.com>
2018-01-13 23:03:41 +0900

    fix test script

--- test.sh
+++ test.sh
@@ -2,9 +2,11 @@
 
 set -e
 
+BASE_IMAGE="canary-for-testing"
 TEST_IMAGE="canary-test"
 TEST_DOCKERFILE="Dockerfile.test"
 
-docker build -t canary . > /dev/null
+docker build -t ${BASE_IMAGE} .
 docker build -f ${TEST_DOCKERFILE} -t ${TEST_IMAGE} .
 docker rmi ${TEST_IMAGE} > /dev/null
+docker rmi ${BASE_IMAGE} > /dev/null
